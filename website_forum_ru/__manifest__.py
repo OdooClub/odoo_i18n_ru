8a84cf558e91fd09e8bf5f4249b07c8e2f129c91
CeleRn <celern84@gmail.com>
2023-07-10 11:43:01 +0300

    [ADD] Add website_forum

---
+++ website_forum_ru/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Odooclub: Russian website forum',
+    'summary': "",
+    'category': 'Website',
+    'version': '1.0',
+    'depends': ['website_forum'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
