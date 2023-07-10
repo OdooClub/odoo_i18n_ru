c9eb627a4037ecd539036648a5158e09bf1c6ae3
CeleRn <celern84@gmail.com>
2023-07-10 19:44:53 +0300

    [ADD] Add website slides

---
+++ website_slides_ru/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Odooclub: Russian website slides',
+    'summary': "",
+    'category': 'Website',
+    'version': '1.0',
+    'depends': ['website_slides'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
